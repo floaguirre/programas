15648bde8d29244217e5ac90a64c432113ce5e84
floaguirre <aguirreflomik@gmail.com>
2016-04-14 16:04:42 -0300

    lalala

---
+++ if.py
@@ -0,0 +1,8 @@
+num1= input ("Ingrese un numero")
+num2= input ("Ingrese otro numero")
+if (num1 > num2):
+ 	print ("El numero %s es mayor a %s,%(num1,num2"))
+else 
+    print("El numero %s es menor a $s",num1,num2)	
+
+ 
