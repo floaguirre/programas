1a7d39bde1f8db978519c3cff19ee2226d04df45
floaguirre <aguirreflomik@gmail.com>
2016-04-14 16:17:00 -0300

    presupuestame

---
+++ presupuesto.py
@@ -0,0 +1,8 @@
+
+monto=input ("Ingrese dinero para presupuestar")
+odontologia= monto * 0.40
+traumatologia= monto * 0.30
+pediatria= monto * 0.30
+print ("El presupuesto para odontologia es:",odontologia)
+print ("El presupuesto para traumatologia es:",traumatologia)
+print ("El presupuesto para pediatria es:",pediatria)
